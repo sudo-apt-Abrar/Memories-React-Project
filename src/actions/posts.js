85207845ed6ccc0aa0453e895e3c666b6450286f
sudo-apt-Abrar <abrarsyed16032001@gmail.com>
2022-06-04 12:23:15 +0530

    commit

---
+++ src/actions/posts.js
@@ -0,0 +1,19 @@
+import * as api from '../api/index.js';
+
+export const getPosts = () => async(dispatch) =>{
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type:'FETCH_ALL', payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }    
+}
+
+export const createPost = (post) => async(dispatch) => {
+    try {
+        const { data } = await api.createPosts(post);
+        dispatch({type:'CREATE', payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
